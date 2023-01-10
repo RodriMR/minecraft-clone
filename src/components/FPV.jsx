00346f6d0df0f31673c38018a9c8542aa253c20f
RodriMR <rodrimr2008@gmail.com>
2023-01-10 20:43:26 -0300

    Ground pixelated texture and camera movement

---
+++ src/components/FPV.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useThree } from "@react-three/fiber";
+import { PointerLockControls } from "@react-three/drei";
+export default function FPV() {
+  const { camera, gl } = useThree();
+  return <PointerLockControls args={[camera, gl.domElement]} />;
+}
--- src/components/Ground.jsx
+++ src/components/Ground.jsx
@@ -1,15 +1,16 @@
 import { usePlane } from "@react-three/cannon";
 import React from "react";
-
+import { groundTexture } from "../images/textures.js";
 export default function Ground() {
   const [ref] = usePlane(() => ({
-    rotation: [0, 0, 0],
-    position: [0, 0, 0],
+    rotation: [-Math.PI / 2, 0, 0],
+    position: [0, -0.2, 0],
   }));
+  groundTexture.repeat.set(100, 100);
   return (
     <mesh ref={ref}>
       <planeBufferGeometry attach={"geometry"} args={[100, 100]} />
-      <meshStandardMaterial attach={"material"} color="green" />
+      <meshStandardMaterial attach={"material"} map={groundTexture} />
     </mesh>
   );
 }
