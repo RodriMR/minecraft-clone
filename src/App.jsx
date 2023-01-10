5312e54200d4d4d2abc13ccefef18c93f782a555
RodriMR <rodrimr2008@gmail.com>
2023-01-10 19:27:02 -0300

    Added sky and ambient light

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import { Canvas } from "@react-three/fiber";
+import { Sky } from "@react-three/drei";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <h1>Minecraft</h1>
-  )
+    <Canvas>
+      <Sky sunPosition={[100, 100, 20]} />
+      <ambientLight intensity={0.5} />
+    </Canvas>
+  );
 }
 
-export default App
+export default App;
