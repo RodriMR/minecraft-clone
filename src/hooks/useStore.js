00ef0a409b010c61cbeda3077b31e88f0c6896c9
RodriMR <rodrimr2008@gmail.com>
2023-01-11 15:49:13 -0300

    cube placement

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -2,19 +2,19 @@ import create from "zustand";
 import { nanoid } from "nanoid";
 export const useStore = create((set) => ({
   texture: "dirt",
-  cubes: [
-    {
-      id: nanoid(),
-      position: [1, 0, 1],
-      texture: "dirt",
-    },
-    {
-      id: nanoid(),
-      position: [1, 1, 1],
-      texture: "log",
-    },
-  ],
-  addCube: () => {},
+  cubes: [],
+  addCube: (x, y, z) => {
+    set((state) => ({
+      cubes: [
+        ...state.cubes,
+        {
+          id: nanoid(),
+          texture: state.texture,
+          position: [x, y, z],
+        },
+      ],
+    }));
+  },
   removeCube: () => {},
   setTexture: () => {},
   saveWorld: () => {},
